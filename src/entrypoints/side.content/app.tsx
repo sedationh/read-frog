719c6b3f273089f42c98fcffd04e36f243d2ef1f
Holden Hu <sedationh@gmail.com>
2025-06-09 15:47:09 +0800

    fix: add translate attribute to main div for improved accessibility

--- src/entrypoints/side.content/app.tsx
+++ src/entrypoints/side.content/app.tsx
@@ -3,7 +3,7 @@ import SideContent from './components/side-content'
 
 export default function App() {
   return (
-    <div className="text-black dark:text-white">
+    <div className="text-black dark:text-white" translate="no">
       <FloatingButton />
       <SideContent />
     </div>
